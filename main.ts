a710aa5c25825fe2e91aa6684b21ddf18013060c
wesleybits <wesley.bitomski@gmail.com>
2023-08-03 19:16:57 -0400

    add fresh for web UI

--- main.ts
+++ main.ts
@@ -1,80 +1,15 @@
-import {Grid} from './src/Grid.ts'
-import {DebugIO} from './src/Io.ts'
-import { RecordingBacktrace } from './src/Backtrace.ts'
+/// <reference no-default-lib="true" />
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
 
-type StepperMode = 'forwards'|'backwards'
+import "$std/dotenv/load.ts";
 
-let filename = ''
-let debug = false
+import { start } from "$fresh/server.ts";
+import manifest from "./fresh.gen.ts";
 
-if (Deno.args.length === 2) {
-    filename = Deno.args[0]
-    debug = Deno.args[1] === 'debug'
-} else if (Deno.args.length === 1) {
-    filename = Deno.args[0]
-} else {
-    console.log('Usage:')
-    console.log('    $ gridly GRIDFILE [debug]')
-    Deno.exit(1)
-}
+import twindPlugin from "$fresh/plugins/twind.ts";
+import twindConfig from "./twind.config.ts";
 
-// check to see if we're given an actually existing file...
-try {
-    await Deno.stat(filename)
-} catch (err) {
-    if (err instanceof Deno.errors.NotFound) {
-        console.log(`File ${filename} is not found`)
-        Deno.exit(2)
-    } else {
-        throw err
-    }
-}
-
-const grid = await Grid.load(filename)
-const debugio = new DebugIO()
-if (debug) {
-    grid.io = debugio
-    grid.backtraceRecorder = new RecordingBacktrace(100)
-}
-
-const e = new TextEncoder()
-const d = new TextDecoder()
-const buf = new Uint8Array(1024)
-let stepperMode: StepperMode = 'forwards'
-while (grid.running) {
-    if (debug) {
-        const g = grid.grid
-        g[grid.position.y][grid.position.x] = '☺'
-
-        debugio.tail(3).forEach(row => {
-            console.log(row)
-        })
-
-        console.log('V:', grid.velocity.toString())
-        console.log('S:', grid.stack)
-        console.log('Mode:', grid.mode)
-        console.log('OPCode:', grid.operator)
-        console.log(g.map(r => r.join('')).join('\n'))
-        Deno.stdout.writeSync(e.encode('...'))
-        Deno.stdin.readSync(buf)
-        const input = d.decode(buf).trim()
-        if (input === '<')
-            stepperMode = 'backwards'
-        if (input === '>')
-            stepperMode = 'forwards'
-    }
-    switch (stepperMode) {
-        case 'forwards':
-            grid.step()
-            break
-        case 'backwards':
-            grid.unstep()
-    }
-    if (debug) {
-        Deno.stdout.writeSync(e.encode('\n'))
-    }
-}
-
-Deno.stdout.writeSync(e.encode('\n'))
-const exitcode = Number(grid.stack.pop() || 0n)
-Deno.exit(exitcode)
+await start(manifest, { plugins: [twindPlugin(twindConfig)] });
---
+++ routes/index.tsx
@@ -0,0 +1,13 @@
+import { Head } from "$fresh/src/runtime/head.ts";
+import { JSX } from "preact";
+
+export default function Index(): JSX.Element {
+    return (<>
+        <Head>
+            <title>Gridly</title>
+        </Head>
+        <body>
+            <h1>Nothing here yet!</h1>
+        </body>
+    </>)
+}
